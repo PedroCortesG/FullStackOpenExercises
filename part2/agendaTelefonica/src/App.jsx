4f6e091b6b625b55486564d7333054c7ca3c6775
PedroCortesG <Pedrodrek@gmail.com>
2025-12-19 23:40:26 -0300

    exercise 2.8

--- part2/agendaTelefonica/src/App.jsx
+++ part2/agendaTelefonica/src/App.jsx
@@ -2,14 +2,18 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas',
+      number: '123'
+     }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addNewPerson = (event) => {
     event.preventDefault()
     const personObject = {
-        name: newName
+        name: newName,
+        number: newNumber
     }
     let invalid = false
     persons.forEach(person => {
@@ -24,8 +28,13 @@ const App = () => {
     }
   }
 
+
   const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
   return (
@@ -38,11 +47,16 @@ const App = () => {
           onChange={handleNameChange}/>
         </div>
         <div>
+          number: <input
+          value = {newNumber}
+          onChange={handleNumberChange}/>
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map((person) => <p key={person.name}>{person.name} {person.number}</p>)}
     </div>
   )
 }
